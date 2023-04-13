a601bd77c89f30458885b35215ad886c8e751b2e
hi-june <wnsgur735@gmail.com>
2023-04-14 00:42:23 +0900

    feat: refresh token 추가

---
+++ voicepocket/src/main/java/com/vp/voicepocket/domain/token/repository/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package com.vp.voicepocket.domain.token.repository;
+
+
+import com.vp.voicepocket.domain.token.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+
+    Optional<RefreshToken> findByKey(Long key);
+}
+
