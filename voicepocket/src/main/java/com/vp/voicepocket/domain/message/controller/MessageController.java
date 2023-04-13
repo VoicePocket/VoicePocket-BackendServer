6a3684e68251805fb70b1410d82fada8e3181f86
hi-june <wnsgur735@gmail.com>
2023-04-13 22:22:55 +0900

    chore: message 모델에 대한 swagger 문서화 추가

--- voicepocket/src/main/java/com/vp/voicepocket/domain/message/controller/MessageController.java
+++ voicepocket/src/main/java/com/vp/voicepocket/domain/message/controller/MessageController.java
@@ -2,22 +2,29 @@ package com.vp.voicepocket.domain.message.controller;
 
 import com.vp.voicepocket.domain.message.model.Message;
 import com.vp.voicepocket.domain.message.service.MessageService;
+import com.vp.voicepocket.global.common.response.model.SingleResult;
+import com.vp.voicepocket.global.common.response.service.ResponseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import javax.validation.Valid;
+
+@RestController
 @RequiredArgsConstructor
+@Tag(name = "Message")
 public class MessageController {
     private final MessageService messageService;
+    private final ResponseService responseService;
 
+    @Operation(summary = "TTS 요청", description = "Text To Speech 서비스를 요청합니다.")
     @PostMapping("/send")
-    @ResponseBody
-    public String send(@RequestBody Message message) {
+    public SingleResult<Message> send(@Valid @RequestBody Message message) {
         messageService.sendMessage(message);
-        return "published!";
+        return responseService.getSingleResult(message);
     }
 }
