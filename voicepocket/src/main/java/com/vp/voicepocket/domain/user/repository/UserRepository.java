fef561e84eca8ededdc81aec51f3d7a86d14832a
hi-june <wnsgur735@gmail.com>
2023-04-14 01:33:59 +0900

    feat: user signup and login

---
+++ voicepocket/src/main/java/com/vp/voicepocket/domain/user/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.vp.voicepocket.domain.user.repository;
+
+
+import com.vp.voicepocket.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("select u from User u where u.userId = :id and u.isActive = true")
+    Optional<User> findById(@Param("id") Long id);
+    @Query("select u from User u where u.email = :email and u.isActive = true")
+    Optional<User> findByEmail(@Param("email") String email);
+}
