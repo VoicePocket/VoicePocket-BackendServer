bb882daae860b0ce42694dd3b828eff808c8eac6
hi-june <wnsgur735@gmail.com>
2023-04-14 01:34:55 +0900

    feat: 인가된 사용자만 TTS 기능을 사용할 수 있도록 제한

--- voicepocket/src/main/java/com/vp/voicepocket/VoicepocketApplication.java
+++ voicepocket/src/main/java/com/vp/voicepocket/VoicepocketApplication.java
@@ -2,6 +2,9 @@ package com.vp.voicepocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class VoicepocketApplication {
@@ -10,4 +13,8 @@ public class VoicepocketApplication {
 		SpringApplication.run(VoicepocketApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
 }
--- voicepocket/src/main/java/com/vp/voicepocket/domain/message/controller/MessageController.java
+++ voicepocket/src/main/java/com/vp/voicepocket/domain/message/controller/MessageController.java
@@ -5,6 +5,9 @@ import com.vp.voicepocket.domain.message.service.MessageService;
 import com.vp.voicepocket.global.common.response.model.SingleResult;
 import com.vp.voicepocket.global.common.response.service.ResponseService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +24,12 @@ public class MessageController {
     private final MessageService messageService;
     private final ResponseService responseService;
 
+    @Parameter(
+            name = "X-AUTH-TOKEN",
+            description = "로그인 성공 후 AccessToken",
+            required = true,
+            schema = @Schema(type = "string"),
+            in = ParameterIn.HEADER)
     @Operation(summary = "TTS 요청", description = "Text To Speech 서비스를 요청합니다.")
     @PostMapping("/send")
     public SingleResult<Message> send(@Valid @RequestBody Message message) {
