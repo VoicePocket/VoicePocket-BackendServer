17644f28f146dc0151657fff589900e18932ec40
hi-june <wnsgur735@gmail.com>
2023-04-22 01:38:16 +0900

    feat: celery task status check api 추가

--- voicepocket/src/main/java/com/vp/voicepocket/domain/message/controller/MessageController.java
+++ voicepocket/src/main/java/com/vp/voicepocket/domain/message/controller/MessageController.java
@@ -1,5 +1,7 @@
 package com.vp.voicepocket.domain.message.controller;
 
+import com.vp.voicepocket.domain.message.exception.CTaskNotFinishedException;
+import com.vp.voicepocket.domain.message.exception.CTaskNotFoundException;
 import com.vp.voicepocket.domain.message.model.Message;
 import com.vp.voicepocket.domain.message.service.MessageService;
 import com.vp.voicepocket.global.common.response.model.SingleResult;
@@ -10,16 +12,14 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "Message")
+@RequestMapping("/api")
 public class MessageController {
     private final MessageService messageService;
     private final ResponseService responseService;
@@ -31,9 +31,39 @@ public class MessageController {
             schema = @Schema(type = "string"),
             in = ParameterIn.HEADER)
     @Operation(summary = "TTS 요청", description = "Text To Speech 서비스를 요청합니다.")
-    @PostMapping("/send")
+    @PostMapping("/tts/send")
     public SingleResult<Message> send(@Valid @RequestBody Message message) {
         messageService.sendMessage(message);
         return responseService.getSingleResult(message);
+    }
+
+    @Parameter(
+            name = "X-AUTH-TOKEN",
+            description = "로그인 성공 후 AccessToken",
+            required = true,
+            schema = @Schema(type = "string"),
+            in = ParameterIn.HEADER)
+    @Operation(summary = "TTS Task id 체크", description = "uuid로 TTS task id를 얻어옵니다.")
+    @GetMapping("/tts/status/uuid/{uuid}")
+    public SingleResult<String> getTaskId(@PathVariable String uuid) {
+        String taskId = messageService.getTaskId(uuid);
+        if (taskId == null) throw new CTaskNotFoundException();
+
+        return responseService.getSingleResult(taskId);
+    }
+
+    @Parameter(
+            name = "X-AUTH-TOKEN",
+            description = "로그인 성공 후 AccessToken",
+            required = true,
+            schema = @Schema(type = "string"),
+            in = ParameterIn.HEADER)
+    @Operation(summary = "TTS Task 체크", description = "task id로 TTS 작업의 진행상태를 봅니다.")
+    @GetMapping("/tts/status/taskId/{taskId}")
+    public SingleResult<String> getTaskStatus(@PathVariable String taskId) {
+        String status = messageService.getTaskStatus(taskId);
+        if (status == null) throw new CTaskNotFinishedException();
+
+        return responseService.getSingleResult(status);
     }
 }
--- voicepocket/src/main/java/com/vp/voicepocket/domain/message/service/MessageService.java
+++ voicepocket/src/main/java/com/vp/voicepocket/domain/message/service/MessageService.java
@@ -1,12 +1,12 @@
 package com.vp.voicepocket.domain.message.service;
 
-
 import com.vp.voicepocket.domain.message.model.Message;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,8 +14,25 @@ import org.springframework.stereotype.Service;
 public class MessageService {
     private static final Logger log = LoggerFactory.getLogger(MessageService.class);
     private final RabbitTemplate rabbitTemplate;    // RabbitTemplate을 통해 Exchange에 메세지를 보내도록 설정
+    private final StringRedisTemplate stringRedisTemplate;
 
     public void sendMessage(Message message) {
         rabbitTemplate.convertAndSend("default", "input.key", message);
+    }
+
+    public String getTaskStatus(String taskId) {
+        String value = stringRedisTemplate.opsForValue().get("celery-task-meta-" + taskId);
+        if (value == null) return null;
+
+        JSONObject json = new JSONObject(value);
+        return json.optString("result");
+    }
+
+    public String getTaskId(String uuid) {
+        String taskId = stringRedisTemplate.opsForValue().get(uuid);
+        if (taskId == null) return null;
+
+        JSONObject json = new JSONObject(taskId);
+        return json.optString("task_id");
     }
 }
