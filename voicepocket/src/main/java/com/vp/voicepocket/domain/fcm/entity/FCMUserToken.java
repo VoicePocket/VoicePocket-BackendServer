2f52183b50c217faee93ac7d5708903a7a2f1eaf
David Lee <tidavid@naver.com>
2023-05-25 19:15:07 +0900

    FEAT: add FCMUserToken Entity

---
+++ voicepocket/src/main/java/com/vp/voicepocket/domain/fcm/entity/FCMUserToken.java
@@ -0,0 +1,28 @@
+package com.vp.voicepocket.domain.fcm.entity;
+
+import com.vp.voicepocket.domain.user.entity.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name="FCMUserToken")
+public class FCMUserToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY, targetEntity = User.class)
+    @JoinColumn(name="UserId", nullable = false, referencedColumnName = "userId")
+    private Long UserId;
+
+    @Column(nullable = false)
+    private String FireBaseToken;
+
+    public void update(String FireBaseToken){
+        this.FireBaseToken = FireBaseToken;
+    }
+}
