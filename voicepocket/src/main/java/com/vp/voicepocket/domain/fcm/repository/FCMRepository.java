c499a1f473ba880ee3a671e8ccbaa0b4cfe680d8
David Lee <tidavid@naver.com>
2023-06-01 18:25:44 +0900

    FIX: fix import in FCMRepository.java

--- voicepocket/src/main/java/com/vp/voicepocket/domain/fcm/repository/FCMRepository.java
+++ voicepocket/src/main/java/com/vp/voicepocket/domain/fcm/repository/FCMRepository.java
@@ -1,13 +1,11 @@
 package com.vp.voicepocket.domain.fcm.repository;
 
 import com.vp.voicepocket.domain.fcm.entity.FCMUserToken;
-import com.vp.voicepocket.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface FCMRepository extends JpaRepository<FCMUserToken, Long> {
