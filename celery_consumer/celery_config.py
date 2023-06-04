859097c968462e5d6939d075a6d4c1c07d08d46a
hi-june <wnsgur735@gmail.com>
2023-06-04 10:31:47 +0900

    feat: synthesizer 관리 방법 redis에서 file로 변경

--- celery_consumer/celery_config.py
+++ celery_consumer/celery_config.py
@@ -2,7 +2,6 @@
 
 # broker_url = 'amqp://sample:sample!@localhost:5672/rabbit_example' # local
 broker_url = 'amqp://sample:sample!@rabbit:5672/rabbit_example'   # docker
-result_backend = 'redis://voicepocket_redis:6379/0' # docker
 task_serializer = 'json'
 result_serializer = 'json'
 accept_content = ['json']
--- celery_consumer/tts_process.py
+++ celery_consumer/tts_process.py
@@ -4,14 +4,9 @@ from TTS.utils.synthesizer import Synthesizer
 from text_process import normalize_text
 from bucket_process import down_model_from_bucket, upload_wav_to_bucket
 
-import redis
-
-# 레디스 연결
-rd = redis.Redis(host='voicepocket_redis', port=6379, db=0)
-
 
 def is_set(email):
-    return rd.get(email) != None
+    return os.path.exists(f'./synth/{email}_synth.p')
 
 def add_synth(email):
     # TODO: best 모델 구성이 결정되면 그에 맞춰 코드를 수정할 것!
@@ -34,14 +29,15 @@ def add_synth(email):
             tts_config_path = "./voice_model_new/config.json"
         )
 
-    serialized_syn = pickle.dumps(synthesizer)
-    rd.set(email, serialized_syn)
+    with open(f'./synth/{email}_synth.p', 'wb') as p:
+        pickle.dump(synthesizer, p)
+
     
 def make_tts(email, uuid, text):
     wav_path = f'./wav/{uuid}.wav'
     
-    serialized_syn = rd.get(email)
-    syn = pickle.loads(serialized_syn)
+    with open(f'./synth/{email}_synth.p', 'rb') as p:
+        syn = pickle.load(p)
 
     symbol = syn.tts_config.characters.characters
     text = normalize_text(text, symbol)
