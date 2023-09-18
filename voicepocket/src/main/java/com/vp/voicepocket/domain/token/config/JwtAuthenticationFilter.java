bc27e5f1380b57fc04a2b29bc8b75cdd3844c6df
KyungMin Lee <hello.tidavid@gmail.com>
2023-09-18 20:12:26 +0900

    FIX: fix MalformedJwtException, ExpiredJwtException Exception Handling

--- voicepocket/src/main/java/com/vp/voicepocket/domain/token/config/JwtAuthenticationFilter.java
+++ voicepocket/src/main/java/com/vp/voicepocket/domain/token/config/JwtAuthenticationFilter.java
@@ -1,37 +1,51 @@
 package com.vp.voicepocket.domain.token.config;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends GenericFilterBean {
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+
     private final JwtProvider jwtProvider;
 
-    // request 로 들어오는 Jwt 의 유효성을 검증 - JwtProvider.validationToken() 을 필터로써 FilterChain 에 추가
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
-        // request header에서 토큰 쿼리값에 해당하는 value값을 통해 토큰을 가져와 검증
-        String token = jwtProvider.resolveToken((HttpServletRequest) request);
-
-        // 검증 관련 로그
-        log.info("[Verifying token]");
-        log.info(((HttpServletRequest) request).getRequestURL().toString());
-
-        if (token != null && jwtProvider.validationToken(token)) {
-            Authentication authentication = jwtProvider.getAuthentication(token);   // 토큰에서 유저 정보를 추출
-            SecurityContextHolder.getContext().setAuthentication(authentication);   // 해당 정보를 SecurityContextHolder에 저장
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        JSONObject jsonObject = new JSONObject();
+        try{
+            String token = jwtProvider.resolveToken(request);
+            if(token != null && jwtProvider.checkValidationToken(token)){
+                Authentication authentication = jwtProvider.getAuthentication(token);   // 토큰에서 유저 정보를 추출
+                SecurityContextHolder.getContext().setAuthentication(authentication);   // 해당 정보를 SecurityContextHolder에 저장
+                filterChain.doFilter(request, response);
+            }
+        } catch (MalformedJwtException e){
+            jsonObject.put("code", -1013);
+            jsonObject.put("message", e.getMessage());
+        } catch ( ExpiredJwtException e){
+            jsonObject.put("code", -1014);
+            jsonObject.put("message", e.getMessage());
+        }catch (UnsupportedJwtException | IllegalArgumentException e){
+            jsonObject.put("code", -1015);
+            jsonObject.put("message", e.getMessage());
+        }finally {
+            response.getWriter().write(jsonObject.toString());
         }
-        filterChain.doFilter(request, response);
+
     }
 }
